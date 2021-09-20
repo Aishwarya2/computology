be34389b1c39638de728fe1a6941616be71a9dc1
Mandadi, Vijay <mandadiv@amazon.com>
2021-09-20 01:59:35 -0700

    loggingAndStateful

--- src/main/java/io/packagecloud/App.java
+++ src/main/java/io/packagecloud/App.java
@@ -1,48 +1,27 @@
 package io.packagecloud;
 
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
+import org.jboss.logging.Logger;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.util.ToolRunner;
-
+import java.util.List;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+import java.util.ArrayList;
 
 /**
  * Hello world!
  *
  */
-public class App extends Configured implements Tool
+@Stateful 
+public class App 
 {
-    public static final String FS_PARAM_NAME = "fs.defaultFS";
-    
-    public int run(String[] args) throws Exception {
-        
-        if (args.length < 2) {
-            System.err.println("HdfsReader [hdfs input path] [local output path]");
-            return 1;
-        }
-        
-        Path inputPath = new Path(args[0]);
-        String localOutputPath = args[1];
-        Configuration conf = getConf();
-        System.out.println("configured filesystem = " + conf.get(FS_PARAM_NAME));
-        FileSystem fs = FileSystem.get(conf);
-        InputStream is = fs.open(inputPath);
-        OutputStream os = new BufferedOutputStream(new FileOutputStream(localOutputPath));
-        IOUtils.copyBytes(is, os, conf);
-        return 0;
-    }
-    
+      
     public static void main( String[] args ) throws Exception {
-        int returnCode = ToolRunner.run(new HdfsReader(), args);
-        System.exit(returnCode);
+        final Logger LOGGER = Logger.getLogger(App.class);
+        LOGGER.error("Configuration file not found.");
+
+        List<String> bookShelf = new ArrayList<String>();
+        bookShelf.add("Test Book Name");
+
     }
-    
+
 }
