5841691b00d3ca6f19028ff052ce36650ba8d197
Mandadi, Vijay <mandadiv@amazon.com>
2021-09-19 19:36:47 -0700

    FSAccess anti pattern

--- src/main/java/io/packagecloud/App.java
+++ src/main/java/io/packagecloud/App.java
@@ -1,5 +1,8 @@
 package io.packagecloud;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static void fsAccessAntipattern() {
+        try {
+            String curDirectory = System.getProperty("user.curDirectory");
+            File dir = new File("tmp/test");
+            dir.mkdir();
+            File tmp = new File(dir, "tmp.txt");
+            tmp.createNewFile();
+            FileOutputStream fout = new FileOutputStream(tmp, true);
+            fout.write(65);
+            fout.close();
+        } catch (Exception ex) {
+            System.out.println( "My bad!" );
+        } finally {
+            System.out.println( "It happens" );
+        }
     }
 }
