c60de79ab90f4c8301689269f2cfeb7eca856ac4
Mandadi, Vijay <mandadiv@amazon.com>
2021-09-20 01:04:34 -0700

    rpc changes

--- src/main/java/io/packagecloud/App.java
+++ src/main/java/io/packagecloud/App.java
@@ -1,5 +1,15 @@
 package io.packagecloud;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.xml.rpc.Stub; 
+import org.apache.soap.rpc;
+
+
 /**
  * Hello world!
  *
@@ -9,6 +19,41 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    static void server() {
+        try {
+            Server obj = new Server();
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Hello", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+        static void client() {
+            String host = "temp";
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    static void tryingSOAP() {
+        service = ServiceFactory.newInstance().createService(new QName(""));
+        Call call = new org.apache.soap.rpc.Call();
+        call = service.createCall();
     }
 
 }
