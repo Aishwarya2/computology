a6568fcc6fb1ca23832764aeddac3f20a391af8f
Mandadi, Vijay <mandadiv@amazon.com>
2021-09-20 01:43:40 -0700

    added cluster

--- src/main/java/io/packagecloud/AppCompute.java
+++ src/main/java/io/packagecloud/AppCompute.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Cluster;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
