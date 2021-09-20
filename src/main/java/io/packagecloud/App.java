706c284d47d420fb552b2739434dc288aeb9541d
Mandadi, Vijay <mandadiv@amazon.com>
2021-09-19 21:17:43 -0700

    Added info

--- src/main/java/io/packagecloud/App.java
+++ src/main/java/io/packagecloud/App.java
@@ -9,6 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String ipv4Address = "192.0.2.146";
+        String ipv6Address = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+
+        System.out.println( "IPV4 - " + ipv4Address + " . IPV6 - " + ipv6Address);
+
     }
 
 }
